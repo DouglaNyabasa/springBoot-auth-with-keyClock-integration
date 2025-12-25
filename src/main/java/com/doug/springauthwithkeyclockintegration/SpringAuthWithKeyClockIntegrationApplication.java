5dc06bb72dbf0373f4061dba89536e15462bb763
douglanyabasa <douglasnyabasa400@gmail.com>
2025-12-25 10:57:15 +0200

    initial commit

---
+++ src/main/java/com/doug/springauthwithkeyclockintegration/SpringAuthWithKeyClockIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.doug.springauthwithkeyclockintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuthWithKeyClockIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAuthWithKeyClockIntegrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/doug/springauthwithkeyclockintegration/SpringAuthWithKeyClockIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doug.springauthwithkeyclockintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthWithKeyClockIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
